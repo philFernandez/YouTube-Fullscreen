6aacac4fd5add9ba0ea32c2f06ee7ac5069a3642
philFernandez <norcal618@live.com>
2016-08-12 15:40:19 -0700

    functional

--- deploy/popup.js
+++ deploy/popup.js
@@ -4,3 +4,20 @@ document.addEventListener('DOMContentLoaded', function() {
 		videos[i].setAttribute('allowfullscreen', '');
 	}
 });
+
+// document.addEventListener('DOMContentLoaded', function() {
+// 	var button = document.getElementById('doIt');
+
+// 	button.addEventListener('click', function() {
+// 		var videos = document.getElementsByTagName('iframe');
+// 		for (i = 0; i < videos.length; i++) {
+// 			videos[i].setAttribute('allowfullscreen', '');
+// 		}
+// 	});
+// });
+
+
+// var videos = document.getElementsByTagName('iframe');
+// for (i = 0; i < videos.length; i++) {
+// 	videos[i].setAttribute('allowfullscreen', '');
+// }
