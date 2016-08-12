3c1cfd6f6aa7360243f51fdb8beb2d81bbedf42e
philFernandez <norcal618@live.com>
2016-08-12 14:58:13 -0700

    folder-struct

--- popup.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-	var videos = document.getElementsByTagName('iframe');
-	for (i = 0; i < videos.length; i++) {
-		videos[i].setAttribute('allowfullscreen', '');
-	}
-});
