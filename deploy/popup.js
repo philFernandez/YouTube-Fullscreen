5adb651e465e95fd21d079bcaa0b1301180b9ab2
philFernandez <norcal618@live.com>
2016-08-12 14:53:07 -0700

    deploy-folder

---
+++ deploy/popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+	var videos = document.getElementsByTagName('iframe');
+	for (i = 0; i < videos.length; i++) {
+		videos[i].setAttribute('allowfullscreen', '');
+	}
+});
